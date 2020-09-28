a79beecc2b97c3ae538d6a85ef6f9bc6be44926e
Dylan Baker <dylang.baker@googlemail.com>
2020-09-28 11:29:04 -0400

    initial commit of ad adapter

---
+++ models/twitter__ad_adapter.sql
@@ -0,0 +1,68 @@
+with accounts as (
+
+    select *
+    from {{ ref('stg_twitter_ads__account_history') }}
+    where is_latest_version = True
+
+), campaigns as (
+
+    select *
+    from {{ ref('stg_twitter_ads__campaign_history') }}
+    where is_latest_version = True
+
+), line_items as (
+
+    select *
+    from {{ ref('stg_twitter_ads__line_item_history') }}
+    where is_latest_version = True
+
+), metrics as (
+
+    select *
+    from {{ ref('stg_twitter_ads__promoted_tweet_report') }}
+
+), promoted_tweet as (
+
+    select *
+    from {{ ref('stg_twitter_ads__promoted_tweet_history')}}
+    where is_latest_version = True
+
+), tweet_url as (
+
+    select *
+    from {{ ref('stg_twitter_ads__tweet_url') }}
+
+), joined as (
+
+    select
+        metrics.date_day,
+        metrics.spend,
+        metrics.clicks,
+        metrics.url_clicks,
+        metrics.impressions,
+        tweet_url.base_url,
+        tweet_url.url_host,
+        tweet_url.url_path,
+        tweet_url.utm_source,
+        tweet_url.utm_medium,
+        tweet_url.utm_campaign,
+        tweet_url.utm_content,
+        tweet_url.utm_term,
+        line_items.name as line_item_name,
+        line_items.line_item_id,
+        campaigns.campaign_name,
+        campaigns.campaign_id
+    from metrics
+    left join promoted_tweet
+        using (promoted_tweet_id)
+    left join tweet_url
+        using (tweet_id)
+    left join line_items
+        using (line_item_id)
+    left join campaigns
+        using (campaign_id)
+
+)
+
+select *
+from joined
